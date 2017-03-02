92c233f98708fb968a5810b708088415e2111958
sdsd4564 <sdsd4564@gmail.com>
2017-03-02 16:50:19 +0900

    170224 2209 Laboratory

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../mysql');
+const http = require('http').Server(router);
+const socketio = require('socket.io')(http);
+
+
+const io = socketio.listen(router);
+io.socket.on('connection', function (socket) {
+    window.onload = function () {
+        const socket = io.connect();
+    };
+});
 
 router.get('/:category/:pid', function (req, res) {
     const pInfo = 'SELECT * FROM product WHERE pid=' + req.params.pid;
@@ -12,7 +22,7 @@ router.get('/:category/:pid', function (req, res) {
             user: req.session,
             product: data
         });
-    })
+    });
 });
 
 module.exports = router;
